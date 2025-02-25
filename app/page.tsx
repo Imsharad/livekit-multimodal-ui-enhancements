73ee2cee7160992db64178104b4a007e802ec87d
Sharad Jain <im.sharad.jain@gmail.com>
2025-02-25 14:30:15 +0530

    [Cursor] Fix duplicate CloseIcon component

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,6 @@ import { EnhancedVoiceAssistant } from "@/components/EnhancedVoiceAssistant";
 import { CloseIcon } from "@/components/CloseIcon";
 import { useKrispNoiseFilter } from "@livekit/components-react/krisp";
 
-// Mock component for demo purposes
-const CloseIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M3.16602 3.16666L12.8327 12.8333M12.8327 3.16666L3.16602 12.8333" stroke="#999999" strokeWidth="1.5" strokeLinecap="square" />
-  </svg>
-);
-
 // Mock type for demo purposes
 type ConnectionDetails = {
   serverUrl: string;
